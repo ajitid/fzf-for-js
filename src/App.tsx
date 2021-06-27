ec74960b867fe10f0505670f3d1ff142d34170a7
Ajit Singh <zlksnkwork@gmail.com>
2021-06-27 20:49:33 +0530

    feat: improve styling

--- src/App.tsx
+++ src/App.tsx
@@ -13,28 +13,67 @@ function App() {
     let result = fzf(list, input);
     result = result.filter((v) => v.result.score !== 0);
     result.sort((a, b) => b.result.score - a.result.score);
+    result = result.slice(0, 12);
     setResult(result);
   };
 
   return (
-    <div>
+    <div className="min-h-screen antialiased break-words px-6 py-4">
       <div>
         <input
+          autoFocus
           value={input}
           onChange={(e) => handleInputChange(e.target.value)}
+          className="py-2 px-3 w-full border-b-2 border-gray-400 outline-none focus:border-purple-500"
+          placeholder="Type to search"
         />
       </div>
-      <div>
-        <ul>
-          {result.map((item) => (
-            <li key={item.item}>
-              {item.item} -- {item.result.score}
-            </li>
-          ))}
-        </ul>
+      <div className="pt-2">
+        {input !== "" ? (
+          <ul>
+            {result.map((item) => (
+              <li key={item.item} className="py-1">
+                <HighlightChars
+                  str={item.item}
+                  highlightIndices={item.pos ?? []}
+                />
+                <span className="text-sm pl-4 italic text-gray-400">
+                  {item.result.score}
+                </span>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <div className="text-gray-600 py-1">
+            List MRU + contextual when query or in frequency (frequent +
+            recency) when input is empty
+          </div>
+        )}
       </div>
     </div>
   );
 }
+
+interface HighlightCharsProps {
+  str: string;
+  highlightIndices: number[];
+}
+
+const HighlightChars = (props: HighlightCharsProps) => {
+  const strArr = props.str.split("");
+  const nodes = strArr.map((v, i) => {
+    if (props.highlightIndices.includes(i)) {
+      return (
+        <span key={i} className="font-semibold">
+          {v}
+        </span>
+      );
+    } else {
+      return v;
+    }
+  });
+
+  return <>{nodes}</>;
+};
 
 export default App;
