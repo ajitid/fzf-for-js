a80b36e490078b5c51d2421fb8f11ad7f580c696
Ajit Singh <zlksnkwork@gmail.com>
2021-07-10 17:50:37 +0530

    feat: use lib mode

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+const { defineConfig } = require("vite");
+const reactRefresh = require("@vitejs/plugin-react-refresh");
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [reactRefresh()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/lib/main.ts"),
+      name: "LibScopeName",
+    },
+    rollupOptions: {
+      external: ["react", "react-router", "react-router-dom", "rmwc"],
+      output: {
+        globals: {
+          react: "React",
+        },
+      },
+    },
+  },
+});
