f5eff5dd257fab183f43013b4ac9d36f42ab38e0
Ajit Singh <zlksnkwork@gmail.com>
2021-07-14 01:15:46 +0530

    choose

--- src/views/basic.tsx
+++ src/views/basic.tsx
@@ -1,11 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import { Fzf, FzfResultItem } from "../lib/main";
-import list from "../lists/date-fns-repo-folders.json";
+import wordList from "../lists/words.json";
+import dateFnDirList from "../lists/date-fns-repo-folders.json";
 
-const fzf = new Fzf(list, {
-  cache: false,
-});
+let fzf = new Fzf(wordList);
 
 export function Basic() {
   const [input, setInput] = useState("");
@@ -25,8 +24,57 @@ export function Basic() {
     setResult(result);
   };
 
+  const [choice, setChoice] = useState("words");
+
+  const selectChoice = (choice: string) => {
+    switch (choice) {
+      case "date-fns":
+        fzf = new Fzf(dateFnDirList);
+        setChoice("date-fns");
+        break;
+      case "words":
+      default:
+        fzf = new Fzf(wordList);
+        setChoice("words");
+    }
+
+    setInput("");
+    setResult([]);
+  };
+
+  useEffect(() => {
+    document.body.classList.add("overflow-y-scroll");
+    return () => document.body.classList.remove("overflow-y-scroll");
+  });
+
   return (
     <div className="min-h-screen antialiased break-words px-6 py-4">
+      <div className="flex justify-end items-center">
+        <input
+          type="radio"
+          id="words"
+          name="list"
+          value="words"
+          checked={choice === "words"}
+          onChange={() => selectChoice("words")}
+          className="mr-1"
+        />
+        <label htmlFor="words" className="mr-3">
+          English words
+        </label>
+        <input
+          type="radio"
+          id="date-fns"
+          name="list"
+          value="date-fns"
+          checked={choice === "date-fns"}
+          onChange={() => selectChoice("date-fns")}
+          className="mr-1"
+        />
+        <label htmlFor="date-fns" className="mr-3">
+          date-fns repo directories
+        </label>
+      </div>
       <div>
         <input
           autoFocus
@@ -53,8 +101,9 @@ export function Basic() {
           </ul>
         ) : (
           <div className="text-gray-600 py-1">
-            List MRU + contextual when query or in frequency (frequent +
-            recency) when input is empty
+            {choice === "date-fns" ? dateFnDirList.length : wordList.length}{" "}
+            items
+            {/* can put last selected items here */}
           </div>
         )}
       </div>
