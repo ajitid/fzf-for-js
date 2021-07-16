f2046d2059896e42145af259871a078aafa0ce00
ajit <zlksnkwork@gmail.com>
2021-07-16 18:36:48 +0530

    add heading link

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { forwardRef, isValidElement } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { MDXProvider } from "@mdx-js/react";
 
@@ -9,6 +9,60 @@ import Docs from "./views/docs.mdx";
 import { Basic } from "./views/basic";
 import { Custom } from "./views/custom";
 import { WithWorker } from "./views/with-worker";
+import linkIconSrc from "./assets/link.svg";
+
+function getAnchor(text: string) {
+  return text
+    .toLowerCase()
+    .replace(/[ \(\.]/g, "-")
+    .replace(/[^a-z0-9-]/g, "");
+}
+
+interface HeadingProps extends React.HTMLAttributes<HTMLHeadingElement> {}
+
+const getHeading = (level: number) => {
+  const Heading = (props: HeadingProps, ref: React.Ref<HTMLHeadingElement>) => {
+    let anchor = getAnchor(
+      typeof props.children === "string" ? getAnchor(props.children) : ""
+    );
+    if (!anchor) {
+      if (isValidElement(props.children)) {
+        const elProps = props.children.props;
+        const children = elProps["children"];
+        if (typeof children === "string") {
+          anchor = getAnchor(children);
+        }
+      }
+    }
+
+    const link = `#${anchor}`;
+
+    return React.createElement(
+      `h${level}`,
+      {
+        id: anchor,
+        ref,
+      },
+      [
+        <a
+          key="1"
+          href={link}
+          className="heading-link"
+          style={{ textDecoration: "none" }}
+        >
+          {props.children}
+          <img
+            src={linkIconSrc}
+            className="w-5 inline-block ml-2"
+            style={{ marginTop: 0, marginBottom: 0 }}
+          />
+        </a>,
+      ]
+    );
+  };
+
+  return forwardRef(Heading);
+};
 
 const mdxComponents = {
   wrapper: (props: any) => (
@@ -17,6 +71,14 @@ const mdxComponents = {
     </div>
   ),
   code: CodeBlock as React.ComponentType<{ children: React.ReactNode }>,
+  // headings
+  ...[2, 3, 4].reduce<Record<string, ReturnType<typeof getHeading>>>(
+    (prev, curr) => {
+      prev[`h${curr}`] = getHeading(curr);
+      return prev;
+    },
+    {}
+  ),
 };
 
 export function App() {
