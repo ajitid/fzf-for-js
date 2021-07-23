2b93778de1cdd106a080e188a859134551f5749a
ajit <zlksnkwork@gmail.com>
2021-07-23 12:47:17 +0530

    feat: loop over list items, extract re-usable part to a fn

--- src/lib/main.ts
+++ src/lib/main.ts
@@ -9,11 +9,7 @@ import type { Result } from "./algo";
 import { Slab, slab } from "./slab";
 import { normalizeRune } from "./normalize";
 import { Casing } from "./types";
-import {
-  buildPatternForExtendedSearch,
-  TermType,
-  termTypeMap,
-} from "./pattern";
+import { buildPatternForExtendedSearch, termTypeMap } from "./pattern";
 import { Int32 } from "./numerics";
 
 interface Options<U> {
@@ -118,70 +114,22 @@ export class Fzf<U> {
         this.opts.normalize,
         query
       );
-
-      // https://github.com/junegunn/fzf/blob/764316a53d0eb60b315f0bbcd513de58ed57a876/src/pattern.go#L354
-      // ^ TODO maybe this helps in caching by not calculating already calculated stuff but whatever
-      const input: {
-        text: Rune[];
-        prefixLength: 0;
-      }[] = [];
-
-      const offsets: Offset[] = [];
-      let totalScore = 0;
-      const allPos = [];
-
-      for (const termSet of pattern.termSets) {
-        let offset: Offset = [0, 0];
-        let currentScore = 0;
-        let matched = false;
-
-        for (const term of termSet) {
-          let algoFn = termTypeMap[term.typ];
-          if (this.opts.algo === "v1") {
-            algoFn = fuzzyMatchV1;
-          }
-          const [off, score, pos] = iter(
-            algoFn,
-            input,
-            term.caseSensitive,
-            // TODO doesn't normalize still come from this.opts??
-            term.normalize,
-            term.text,
-            slab
-          );
-
-          const sidx = off[0];
-          if (sidx >= 0) {
-            if (term.inv) {
-              continue;
-            }
-
-            offset = off;
-            currentScore = score;
-            matched = true;
-
-            if (pos !== null) {
-              allPos.push(...pos);
-            } else {
-              for (let idx = off[0]; idx < off[1]; ++idx) {
-                // idx is typecasted to int
-                allPos.push(idx);
-              }
-            }
-            break;
-          } else if (term.inv) {
-            offset = [0, 0];
-            currentScore = 0;
-            matched = true;
-            continue;
-          }
+      for (const [idx, runes] of this.runesList.entries()) {
+        const stuff = v2stuff(runes, pattern, this.opts.algo);
+        let sidx = -1,
+          eidx = -1;
+        if (stuff.offsets.length > 0) {
+          sidx = stuff.offsets[0][0];
+          eidx = stuff.offsets[0][1];
         }
-        if (matched) {
-          offsets.push(offset);
-          totalScore += currentScore;
-        }
+        result.push({
+          score: stuff.totalScore,
+          item: this.items[idx],
+          positions: stuff.allPos,
+          start: sidx,
+          end: eidx,
+        });
       }
-      // offsets, totalScore, allPos
     } else {
       result = this.basicMatch(query);
     }
@@ -272,4 +220,80 @@ function iter(
     }
   }
   return [[-1, -1], 0, null];
+}
+
+function v2stuff(
+  text: Rune[],
+  pattern: ReturnType<typeof buildPatternForExtendedSearch>,
+  fuzzyAlgo: "v1" | "v2"
+) {
+  // https://github.com/junegunn/fzf/blob/764316a53d0eb60b315f0bbcd513de58ed57a876/src/pattern.go#L354
+  // ^ TODO maybe this helps in caching by not calculating already calculated stuff but whatever
+  const input: {
+    text: Rune[];
+    prefixLength: number;
+  }[] = [
+    {
+      text: text,
+      prefixLength: 0,
+    },
+  ];
+
+  const offsets: Offset[] = [];
+  let totalScore = 0;
+  const allPos = [];
+
+  for (const termSet of pattern.termSets) {
+    let offset: Offset = [0, 0];
+    let currentScore = 0;
+    let matched = false;
+
+    for (const term of termSet) {
+      let algoFn = termTypeMap[term.typ];
+      if (fuzzyAlgo === "v1") {
+        algoFn = fuzzyMatchV1;
+      }
+      const [off, score, pos] = iter(
+        algoFn,
+        input,
+        term.caseSensitive,
+        // TODO doesn't normalize still come from this.opts??
+        term.normalize,
+        term.text,
+        slab
+      );
+
+      const sidx = off[0];
+      if (sidx >= 0) {
+        if (term.inv) {
+          continue;
+        }
+
+        offset = off;
+        currentScore = score;
+        matched = true;
+
+        if (pos !== null) {
+          allPos.push(...pos);
+        } else {
+          for (let idx = off[0]; idx < off[1]; ++idx) {
+            // idx is typecasted to int
+            allPos.push(idx);
+          }
+        }
+        break;
+      } else if (term.inv) {
+        offset = [0, 0];
+        currentScore = 0;
+        matched = true;
+        continue;
+      }
+    }
+    if (matched) {
+      offsets.push(offset);
+      totalScore += currentScore;
+    }
+  }
+
+  return { offsets, totalScore, allPos };
 }
