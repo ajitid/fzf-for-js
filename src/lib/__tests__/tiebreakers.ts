7bb2a1c95ded5e1defce1cf5eff4f0f0020b2fd2
ajit <zlksnkwork@gmail.com>
2021-07-27 20:52:47 +0530

    test: tiebreakers.ts

---
+++ src/lib/__tests__/tiebreakers.ts
@@ -0,0 +1,30 @@
+import { Fzf } from "../main";
+import { tiebreakers } from "../tiebreakers";
+
+test("byLengthAsc", () => {
+  const list = ["aaaaa", "caaaaaaaa", "baaaaa"];
+
+  const fzf = new Fzf(list, {
+    tiebreakers: [tiebreakers.byLengthAsc],
+  });
+
+  expect(fzf.find("aa").map((entry) => entry.item)).toMatchObject([
+    "aaaaa",
+    "baaaaa",
+    "caaaaaaaa",
+  ]);
+});
+
+test("byStartAsc", () => {
+  const list = ["aaaa", "ccaaaa", "baaa"];
+
+  const fzf = new Fzf(list, {
+    tiebreakers: [tiebreakers.byStartAsc],
+  });
+
+  expect(fzf.find("aa").map((entry) => entry.item)).toMatchObject([
+    "aaaa",
+    "baaa",
+    "ccaaaa",
+  ]);
+});
