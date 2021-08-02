95708064b6729384230e7555ff4d72277fecbd43
ajit <zlksnkwork@gmail.com>
2021-08-03 03:28:53 +0530

    chore: don't allow docs for versions <=0.3 to be written as they are incompatible

--- scripts/fill-with-old-docs.js
+++ scripts/fill-with-old-docs.js
@@ -4,11 +4,11 @@ const currVerStr = require("../package.json").version;
 const currVerMajor = parseInt(currVerStr.split(".")[0], 10);
 const currVerMinor = parseInt(currVerStr.split(".")[1], 10);
 
-shell.rm("-r", "src/docs/views/old-docs");
-shell.mkdir("src/docs/views/old-docs");
-shell.touch("src/docs/views/old-docs/.gitkeep");
+shell.rm("-r", "src/docs/old-docs");
+shell.mkdir("src/docs/old-docs");
+shell.touch("src/docs/old-docs/.gitkeep");
 
-const writeFile = (verMajor, verMinor) => {
+const writeFileLegacy = (verMajor, verMinor) => {
   const nextCommitHash = shell
     .exec(
       `git log -S '"version": "${verMajor}.${verMinor}' --oneline -n 1 --branches HEAD -- package.json`,
@@ -31,6 +31,33 @@ const writeFile = (verMajor, verMinor) => {
   if (fileContent !== "") {
     shell.ShellString(fileContent).to(`src/docs/views/old-docs/${verStr}.mdx`);
   }
+};
+
+const writeFile = (verMajor, verMinor) => {
+  if (verMajor === 0 && verMinor <= 3) {
+    console.log("nope won't write it", verMajor, verMinor);
+    // writeFileLegacy(verMajor, verMinor);
+  }
+
+  const nextCommitHash = shell
+    .exec(
+      `git log -S '"version": "${verMajor}.${verMinor}' --oneline -n 1 --branches HEAD -- package.json`,
+      {
+        silent: true,
+      }
+    )
+    .stdout.split(" ")[0];
+
+  const verStr = JSON.parse(
+    shell.exec(`git show ${nextCommitHash}^:./package.json`, { silent: true })
+      .stdout
+  )["version"];
+
+  shell.mkdir("src/docs/old-docs/" + verStr);
+
+  shell.exec(
+    `git --work-tree=src/docs/old-docs/${verStr} checkout ${nextCommitHash}^ -- src`
+  );
 };
 
 for (let i = currVerMajor; i >= Math.max(0, currVerMajor - 2); i--) {
