7bfded3ff3733cb47b5f2c89825bedda2cb45c84
ajit <zlksnkwork@gmail.com>
2021-08-03 23:54:25 +0530

    chore: add support to view legacy (<=0.3.0) docs

--- scripts/fill-with-old-docs.js
+++ scripts/fill-with-old-docs.js
@@ -8,11 +8,49 @@ shell.rm("-r", "src/docs/old-docs");
 shell.mkdir("src/docs/old-docs");
 shell.touch("src/docs/old-docs/.gitkeep");
 
-const writeFile = (verMajor, verMinor) => {
-  if (verMajor === 0 && verMinor <= 3) {
-    console.log(
-      `v${verMajor}.${verMinor} is incompatible with the current docs structure. Skipping...`
+const fillLegacyDocs = (verMajor, verMinor) => {
+  const nextCommitHash = shell
+    .exec(
+      `git log -S '"version": "${verMajor}.${verMinor}' --oneline -n 1 --branches HEAD -- package.json`,
+      {
+        silent: true,
+      }
+    )
+    .stdout.split(" ")[0];
+
+  const verStr = JSON.parse(
+    shell.exec(`git show ${nextCommitHash}^:./package.json`, { silent: true })
+      .stdout
+  )["version"];
+
+  const fileContent = shell.exec(
+    `git show ${nextCommitHash}^:./src/views/docs.mdx`,
+    { silent: true }
+  ).stdout;
+
+  const pkgFileContent = shell.exec(
+    `git show ${nextCommitHash}^:./package.json`,
+    { silent: true }
+  ).stdout;
+
+  if (fileContent !== "") {
+    shell.mkdir("-p", `src/docs/old-docs/${verStr}/src/docs`);
+    shell
+      .ShellString(fileContent)
+      .to(`src/docs/old-docs/${verStr}/src/docs/docs.mdx`);
+    shell
+      .ShellString(pkgFileContent)
+      .to(`src/docs/old-docs/${verStr}/package.json`);
+    shell.cp(
+      "scripts/legacy-app-routes.tsx",
+      `src/docs/old-docs/${verStr}/src/docs/app-routes.tsx`
     );
+  }
+};
+
+const fillDocs = (verMajor, verMinor) => {
+  if (verMajor === 0 && verMinor <= 3) {
+    fillLegacyDocs(verMajor, verMinor);
     return;
   }
 
@@ -45,7 +83,7 @@ const writeFile = (verMajor, verMinor) => {
 for (let i = currVerMajor; i >= Math.max(0, currVerMajor - 2); i--) {
   if (i === currVerMajor) {
     for (let j = currVerMinor - 1; j >= Math.max(0, currVerMinor - 2); j--) {
-      writeFile(i, j);
+      fillDocs(i, j);
     }
   } else {
     const nextCommitHash = shell
@@ -64,7 +102,7 @@ for (let i = currVerMajor; i >= Math.max(0, currVerMajor - 2); i--) {
     const verMinor = parseInt(verStr.split(".")[1], 10);
 
     for (let j = verMinor; j >= Math.max(0, verMinor - 2); j--) {
-      writeFile(i, j);
+      fillDocs(i, j);
     }
   }
 }
