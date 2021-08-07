f925c8ae02fda9652a2a2187d1d73008c5fff65c
ajit <zlksnkwork@gmail.com>
2021-08-07 14:37:19 +0530

    fix: positions usage in example

--- src/docs/components/highlight-chars.tsx
+++ src/docs/components/highlight-chars.tsx
@@ -2,13 +2,13 @@ import React from "react";
 
 interface HighlightCharsProps {
   str: string;
-  highlightIndices: number[];
+  highlightIndices: Set<number>;
 }
 
 export const HighlightChars = (props: HighlightCharsProps) => {
   const strArr = props.str.split("");
   const nodes = strArr.map((v, i) => {
-    if (props.highlightIndices.includes(i)) {
+    if (props.highlightIndices.has(i)) {
       return (
         <span key={i} className="font-semibold">
           {v}
