da65145439b9c31a08abd570b495d23bbed2065f
ajit <zlksnkwork@gmail.com>
2021-08-10 00:25:59 +0530

    docs: rename variable

--- src/docs/components/highlight-chars.tsx
+++ src/docs/components/highlight-chars.tsx
@@ -2,13 +2,13 @@ import React from "react";
 
 interface HighlightCharsProps {
   str: string;
-  highlightIndices: Set<number>;
+  indices: Set<number>;
 }
 
 export const HighlightChars = (props: HighlightCharsProps) => {
   const strArr = props.str.split("");
   const nodes = strArr.map((v, i) => {
-    if (props.highlightIndices.has(i)) {
+    if (props.indices.has(i)) {
       return (
         <span key={i} className="font-semibold">
           {v}
