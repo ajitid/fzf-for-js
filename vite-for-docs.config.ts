26860364068684adc5db74e4211372e432b412e1
ajit <zlksnkwork@gmail.com>
2022-04-26 10:13:51 +0530

    build: upgrade to MDX 2

--- vite-for-docs.config.ts
+++ vite-for-docs.config.ts
@@ -1,12 +1,23 @@
 import { defineConfig } from "vite";
 import reactRefresh from "@vitejs/plugin-react-refresh";
-import mdx from "vite-plugin-mdx";
 import macrosPlugin from "vite-plugin-babel-macros";
 
-export default defineConfig({
-  plugins: [reactRefresh(), mdx(), macrosPlugin()],
-  define: {
-    // this is required for macrosPlugin to work
-    "process.env": {},
-  },
+export default defineConfig(async () => {
+  // changes would be needed if you upgrade to React v18, see
+  // https://github.com/brillout/vite-plugin-mdx/issues/44#issuecomment-974540152
+  const mdx = await import("@mdx-js/rollup");
+
+  return {
+    plugins: [
+      reactRefresh(),
+      mdx.default({
+        providerImportSource: "@mdx-js/react",
+      }),
+      macrosPlugin(),
+    ],
+    define: {
+      // this is required for macrosPlugin to work
+      "process.env": {},
+    },
+  };
 });
