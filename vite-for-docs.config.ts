82c9ae5d7f88f602c4adaa3d8c697029dca4038c
ajit <zlksnkwork@gmail.com>
2022-04-26 10:36:11 +0530

    docs: make table work with mdxv2

--- vite-for-docs.config.ts
+++ vite-for-docs.config.ts
@@ -5,13 +5,15 @@ import macrosPlugin from "vite-plugin-babel-macros";
 export default defineConfig(async () => {
   // changes would be needed if you upgrade to React v18, see
   // https://github.com/brillout/vite-plugin-mdx/issues/44#issuecomment-974540152
-  const mdx = await import("@mdx-js/rollup");
+  const mdx = await import("@mdx-js/rollup").then((mod) => mod.default);
+  const remarkGfm = await import("remark-gfm").then((mod) => mod.default);
 
   return {
     plugins: [
       reactRefresh(),
-      mdx.default({
+      mdx({
         providerImportSource: "@mdx-js/react",
+        remarkPlugins: [remarkGfm],
       }),
       macrosPlugin(),
     ],
