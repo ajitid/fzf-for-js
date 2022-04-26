d104ebda303e7dbd7cd1f87b4a2967c4198662d0
ajit <zlksnkwork@gmail.com>
2022-04-27 04:43:16 +0530

    build: upgrade tailwind to v3 (#108)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx,mdx}"],
-  darkMode: false, // or 'media' or 'class'
+  content: ["./index.html", "./src/docs/**/*.{vue,js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       typography: {
