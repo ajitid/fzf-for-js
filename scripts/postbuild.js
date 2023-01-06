cf28223dd3d4ec2dceab0bcc0906dcb5965021ba
Ajit <zlksnkwork@gmail.com>
2023-01-06 09:35:03 +0530

    build: hard code year

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -5,8 +5,7 @@ const pkg = require("../package.json");
 const LIBRARY_NAME = pkg.name;
 const VERSION = pkg.version;
 const AUTHOR = pkg.author;
-const currYear = new Date().getFullYear();
-const YEAR = currYear === 2021 ? "2021" : `2021-${currYear}`;
+const YEAR = "2021";
 
 // Intentionally using a script instead of using rollup output option for banner
 // because for UMD builds the banner wasn't appearing at the top.
